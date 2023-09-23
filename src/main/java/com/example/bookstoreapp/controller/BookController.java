e496a8cbd58ae5dfc9cbb8cbaf0d43d95dfdbbe9
vrnvv <voropiano@gmail.com>
2023-09-23 03:20:57 +0300

    add annotation method security and add access by role

--- src/main/java/com/example/bookstoreapp/controller/BookController.java
+++ src/main/java/com/example/bookstoreapp/controller/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class BookController {
         return ResponseEntity.ok(books);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     @Operation(summary = "Create books")
     public ResponseEntity<BookDto> createBook(@RequestBody @Valid BookRequestDto bookDto) {
@@ -57,6 +59,7 @@ public class BookController {
         }
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     @Operation(summary = "Update book", description = "Update book by id")
     public ResponseEntity<BookDto> updateBook(@PathVariable Long id,
@@ -70,6 +73,7 @@ public class BookController {
         }
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete book", description = "Delete book by id")
